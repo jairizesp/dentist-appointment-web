17274d0b636230bb8ff76e7c6f1b85d2cbd7ea4d
jairizesps <jesposo@ardisdata.com>
2025-04-08 01:30:18 +0800

    forcely cast to string

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CLIENT_URL,
+    origin: process.env.CLIENT_URL!,
     credentials: true,
   });
 
