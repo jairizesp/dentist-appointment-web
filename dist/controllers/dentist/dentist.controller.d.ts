193cadc5a816fce71a9d71aa59032c907b4283a3
jairizesps <jesposo@ardisdata.com>
2025-04-08 03:10:07 +0800

    remove dis

--- dist/controllers/dentist/dentist.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { DentistService } from 'src/services/dentist/dentist.service';
-export declare class DentistController {
-    private readonly dentistService;
-    constructor(dentistService: DentistService);
-    findAll(): Promise<import("../../interface/dentist/dentist.interface").DentistInformation[]>;
-    findOne(id: number): Promise<import("../../interface/dentist/dentist.interface").DentistInformation>;
-}
--- dist/services/dentist/dentist.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Pool } from 'pg';
-import { DentistInformation } from 'src/interface/dentist/dentist.interface';
-export declare class DentistService {
-    private readonly pool;
-    constructor(pool: Pool);
-    findAll(): Promise<DentistInformation[]>;
-    findOne(id: number): Promise<DentistInformation>;
-}
