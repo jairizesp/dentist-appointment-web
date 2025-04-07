cae699d01e75e05965222b5f08eb3ab6523c156e
jairizesps <jesposo@ardisdata.com>
2025-04-08 02:10:43 +0800

    add dist folder

---
+++ dist/controllers/appointment/appointment.controller.js
@@ -0,0 +1,82 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppointmentController = void 0;
+const common_1 = require("@nestjs/common");
+const appointment_service_1 = require("../../services/appointment/appointment.service");
+let AppointmentController = class AppointmentController {
+    appointmentService;
+    constructor(appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+    async getAppointmentByDentist(query) {
+        return this.appointmentService.getAppointmentByDentist({
+            dentist_id: Number(query.dentist_id),
+            appointment_date: query.appointment_date,
+        });
+    }
+    async addAppointment(payload) {
+        return this.appointmentService.addAppointment(payload);
+    }
+    async getAppointmentByPatient(id) {
+        return this.appointmentService.getAppointmentByPatient(Number(id));
+    }
+    async getPreviousAppointmentByPatient(id) {
+        return this.appointmentService.getPreviousAppointmentByPatient(Number(id));
+    }
+    async cancelAppointment(id) {
+        return this.appointmentService.cancelAppointment(Number(id));
+    }
+};
+exports.AppointmentController = AppointmentController;
+__decorate([
+    (0, common_1.Get)('/by-dentist'),
+    __param(0, (0, common_1.Query)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], AppointmentController.prototype, "getAppointmentByDentist", null);
+__decorate([
+    (0, common_1.Post)('/add-appointment'),
+    __param(0, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", Promise)
+], AppointmentController.prototype, "addAppointment", null);
+__decorate([
+    (0, common_1.Get)('/by-patient/upcoming/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", Promise)
+], AppointmentController.prototype, "getAppointmentByPatient", null);
+__decorate([
+    (0, common_1.Get)('/by-patient/history/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Number]),
+    __metadata("design:returntype", Promise)
+], AppointmentController.prototype, "getPreviousAppointmentByPatient", null);
+__decorate([
+    (0, common_1.Put)('/cancel-appointment/:id'),
+    __param(0, (0, common_1.Param)('id')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [String]),
+    __metadata("design:returntype", Promise)
+], AppointmentController.prototype, "cancelAppointment", null);
+exports.AppointmentController = AppointmentController = __decorate([
+    (0, common_1.Controller)('api/appointment'),
+    __metadata("design:paramtypes", [appointment_service_1.AppointmentService])
+], AppointmentController);
+//# sourceMappingURL=appointment.controller.js.map
---
+++ dist/services/appointment/appointment.service.js
@@ -0,0 +1,97 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+var _a;
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppointmentService = void 0;
+const common_1 = require("@nestjs/common");
+const pg_1 = require("pg");
+let AppointmentService = class AppointmentService {
+    pool;
+    constructor(pool) {
+        this.pool = pool;
+    }
+    async getAppointmentByDentist(query) {
+        const result = await this.pool.query('SELECT * FROM appointments WHERE dentist_id = $1 AND appointment_date = $2', [query.dentist_id, query.appointment_date]);
+        return result.rows;
+    }
+    async addAppointment(payload) {
+        const appointment_date = payload.appointment_date.toString()?.split('T')[0];
+        const response = await this.pool.query('INSERT INTO appointments(dentist_id, user_id, appointment_date, "from", "to") VALUES($1, $2, $3, $4, $5) RETURNING *', [
+            payload.dentist_id,
+            payload.user_id,
+            appointment_date,
+            payload.from,
+            payload.to,
+        ]);
+        const appointment = response.rows[0];
+        return appointment;
+    }
+    async getAppointmentByPatient(user_id) {
+        const result = await this.pool.query(`SELECT 
+        a.*, 
+        d.first_name, 
+        d.last_name, 
+        d.specialization 
+      FROM 
+        appointments a
+      LEFT JOIN 
+        dentist d ON d.id = a.dentist_id
+      WHERE 
+        a.user_id = $1
+        AND  (a.appointment_date > CURRENT_DATE
+      OR (a.appointment_date = CURRENT_DATE AND (
+          CASE 
+            WHEN a.from < 8 THEN a.from + 12
+            ELSE a.from
+          END
+        ) > EXTRACT(HOUR FROM CURRENT_TIMESTAMP)))
+        AND a.is_cancelled = FALSE
+        ORDER BY a.appointment_date ASC;`, [user_id]);
+        const adjustedResults = result.rows.map((row) => {
+            row.appointment_date = new Date(row.appointment_date).toLocaleDateString();
+            return row;
+        });
+        return adjustedResults;
+    }
+    async getPreviousAppointmentByPatient(user_id) {
+        const result = await this.pool.query(`SELECT a.*, d.first_name, d.last_name, d.specialization 
+      FROM appointments a
+      LEFT JOIN dentist d ON d.id = a.dentist_id
+      WHERE a.user_id = $1 
+      AND ( a.appointment_date < CURRENT_DATE
+      OR (a.appointment_date = CURRENT_DATE AND (
+          CASE 
+            WHEN a.from < 8 THEN a.from + 12
+            ELSE a.from
+          END
+        ) < EXTRACT(HOUR FROM CURRENT_TIMESTAMP)))`, [user_id]);
+        return result.rows;
+    }
+    async cancelAppointment(id) {
+        try {
+            const result = await this.pool.query('UPDATE appointments SET is_cancelled = TRUE WHERE id = $1 RETURNING *', [id]);
+            return result.rows[0];
+        }
+        catch (error) {
+            throw new common_1.InternalServerErrorException('Something went wrong');
+        }
+    }
+};
+exports.AppointmentService = AppointmentService;
+exports.AppointmentService = AppointmentService = __decorate([
+    (0, common_1.Injectable)(),
+    __param(0, (0, common_1.Inject)('PG_POOL')),
+    __metadata("design:paramtypes", [typeof (_a = typeof pg_1.Pool !== "undefined" && pg_1.Pool) === "function" ? _a : Object])
+], AppointmentService);
+//# sourceMappingURL=appointment.service.js.map
